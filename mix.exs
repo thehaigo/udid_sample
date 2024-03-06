6c1b629a2737f3b700e62d189fa4077eee3a3cb4
thehaigo <sirannha@gmail.com>
2024-03-07 00:17:53 +0900

    fix mix

--- mix.exs
+++ mix.exs
@@ -57,7 +57,7 @@ defmodule Sample.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:desktop_setup, path: "../desktop_setup"}
+      {:desktop_setup, github: "thehaigo/desktop_setup", only: :dev}
     ]
   end
 
